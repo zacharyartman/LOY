db47d62df38c806b6697b2b5913f42727bea6c35
Zachary Artman <zacharyartman@icloud.com>
2024-08-10 11:22:38 -0700

    fixed blog layout

--- components/Blog/BlogItem.tsx
+++ components/Blog/BlogItem.tsx
@@ -28,7 +28,7 @@ const BlogItem = ({ blog }: { blog: Blog }) => {
         className="animate_top rounded-lg bg-white p-4 pb-9 shadow-solid-8 dark:bg-blacksection"
       >
         <Link href={`/blog/`} className="relative block aspect-[368/239]">
-          <Image src={mainImage} alt={title} fill />
+          <Image src={mainImage} alt={title} layout="fill" />
         </Link>
 
         <div className="px-4">
