64d0c2800ef5f86fdc4f68c9442ab1e745515c76
Zachary Artman <zacharyartman@icloud.com>
2024-08-11 08:12:16 -0700

    updated margin for popup

--- components/Popup/index.tsx
+++ components/Popup/index.tsx
@@ -40,7 +40,7 @@ export default function Popup() {
             id="modal-background"
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my-6 mx-8 max-w-xl">
+            <div className="relative w-auto mt-20 mx-8 max-w-xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/* header */}
                 <div className="flex items-center flex-col justify-between p-0 border-b border-solid border-blueGray-200 rounded-t">
