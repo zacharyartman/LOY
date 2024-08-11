d86dc496dade47980510e75bb8d3775338e8fa24
Zachary Artman <zacharyartman@icloud.com>
2024-08-11 09:31:23 -0700

    Fixed popup sizing

--- components/Popup/index.tsx
+++ components/Popup/index.tsx
@@ -40,34 +40,36 @@ export default function Popup() {
             id="modal-background"
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto mt-20 mx-8 max-w-xl">
+            <div className="relative w-auto mt-20 mx-8 max-w-lg">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/* header */}
-                <div className="flex items-center flex-col justify-between p-0 border-b border-solid border-blueGray-200 rounded-t">
+                <div className="flex items-center flex-col justify-between p-0 rounded-t">
 
-                  <div className="relative h-50 sm:h-40 w-full mb-2 mx-auto rounded-t-lg overflow-hidden">
+                  <div className="relative h-30 sm:h-40 w-full mb-2 mx-auto rounded-t-lg overflow-hidden">
                     <Image src={"/images/general/teaching.jpeg"} alt={'TODO'} layout="fill" objectFit="cover" />
                   </div>
+                  <div className="border-b border-solid border-blueGray-200 mx-0 md:mx-15">
+                  <div className="px-5 md:px-0 pt-3 pb-5">
+                    <h3 className="text-lg font-semibold text-black text-center dark:text-white xl:text-2xl">
+                      New Student Special: Only $79 for Three Weeks Unlimited Yoga Classes
+                    </h3>
+                  </div>
+                  </div>
                   <button
-                    className="absolute top-0 right-0 m-2 px-2 rounded-full bg-opacity-75 text-black text-2xl font-semibold bg-white backgroundfocus:outline-none"
+                    className="absolute top-0 right-0 m-2 px-2 rounded-full bg-opacity-75 text-primary text-2xl font-semibold bg-white backgroundfocus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
                     X
                   </button>
-                  <div className="px-7.5 pt-5 pb-7">
-                    <h3 className="text-xl font-semibold text-black text-center dark:text-white xl:text-itemtitle">
-                      New Student Special: Only $79 for Three Weeks Unlimited Yoga Classes
-                    </h3>
-                  </div>
                 </div>
                 {/* body */}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-primary text-title lg:text-lg leading-relaxed">
+                <div className="relative py-5 px-8 md:px-15 flex-auto">
+                  <p className="my-0 text-sectiontitle leading-relaxed">
                     Enjoy three weeks of unlimited yoga for only $79. Your three weeks doesn’t start until your first class giving you the freedom to purchase now for use at a later date (new local students only).
                   </p>
                 </div>
                 {/* footer */}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                <div className="flex items-center justify-end px-6 py-4 mx-0 md:mx-15 border-t border-solid border-blueGray-200 rounded-b">
                   <a
                     href="https://momence.com/Las-Olas-Yoga/membership/Three-Weeks-for-%2479.00-(New-Local-Students-Only)/136483"
                     className="flex items-center justify-center rounded-full bg-primary px-10.5 py-2.5 w-full text-regular text-white transition-all duration-200 hover:bg-primaryho"
