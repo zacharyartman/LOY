0affec6699f06136289d28a4458a87af08ca835a
Zachary Artman <zacharyartman@icloud.com>
2024-08-11 10:31:01 -0700

    updated favicon

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,12 +1,16 @@
-import MomenceChat from "@/components/MomenceChat";
-
 export default function Head() {
   return (
     <>
       <title> Las Olas Yoga - Fort Lauderdale Yoga Studio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="TODO" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+      <meta name="msapplication-TileColor" content="#da532c"/>
+      <meta name="theme-color" content="#ffffff"/>
     </>
   );
 }
