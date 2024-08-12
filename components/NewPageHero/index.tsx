e564cf5614559633ff1004e55710d9dc721f6993
Zachary Artman <zacharyartman@icloud.com>
2024-08-12 11:28:04 -0700

    Fixed hero background glitch

--- components/NewPageHero/index.tsx
+++ components/NewPageHero/index.tsx
@@ -8,7 +8,7 @@ const NewPageHero = ({titleText, link, position='bg-center'}) => {
 
   return (
     <>
-      <section className={`overflow-hidden pb-20 mt-[90px] pt-20 xl:pb-25 bg-cover bg-center h-100 ${position}` }
+      <section className={`overflow-hidden pb-20 mt-[90px] pt-20 xl:pb-25 bg-cover h-100 ${position}` }
       style={backgroundImageStyle}>
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <h1 className="pt-2 xl:pt-8 text-3xl font-bold text-white dark:text-white text-hero pr-0 text-center">
