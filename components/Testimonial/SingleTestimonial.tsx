e292d70b79f423423f4505a8aadef36d821125f5
Zachary Artman <zacharyartman@icloud.com>
2024-08-12 09:31:37 -0700

    adjusted width of teachers page

--- components/Testimonial/SingleTestimonial.tsx
+++ components/Testimonial/SingleTestimonial.tsx
@@ -9,53 +9,53 @@ const SingleTestimonial = ({ review }: { review: Testimonial }) => {
 
   return (
     <motion.div
-    variants={{
-      hidden: {
-        opacity: 0,
-        y: -20,
-      },
-      visible: {
-        opacity: 1,
-        y: 0,
-      },
-    }}
-    initial="hidden"
-    whileInView="visible"
-    transition={{ duration: 1, delay: 0.1 }}
-    viewport={{ once: true }}
-    className="animate_top">
+      variants={{
+        hidden: {
+          opacity: 0,
+          y: -20,
+        },
+        visible: {
+          opacity: 1,
+          y: 0,
+        },
+      }}
+      initial="hidden"
+      whileInView="visible"
+      transition={{ duration: 1, delay: 0.1 }}
+      viewport={{ once: true }}
+      className="animate_top">
 
-    <div className="rounded-lg bg-white pt-7.5 shadow-solid-9 dark:border dark:border-strokedark dark:bg-blacksection dark:shadow-none">
-      <div className="px-12 py-5 flex justify-center">
-        <Image src={image} className="rounded-full" width={175} height={175} />
-      </div>
-      <div className="px-10">
-        <div className="mb-5 text-center border-b border-stroke dark:border-strokedark">
-          <div>
-            <h2 className="mb-1.5 text-3xl text-black dark:text-white">
+      <div className="rounded-lg bg-white pt-7.5 shadow-solid-9 dark:border dark:border-strokedark dark:bg-blacksection dark:shadow-none">
+        <div className="px-12 py-5 flex justify-center">
+          <Image src={image} className="rounded-full" width={175} height={175} />
+        </div>
+        <div className="px-10">
+          <div className="mb-5 text-center border-b border-stroke dark:border-strokedark">
+            <div>
+              <h2 className="mb-1.5 text-3xl text-black dark:text-white">
                 {name}
-            </h2>
-            <div
-              className={`relative transition-max-height duration-500 ease-in-out ${showFullContent ? "max-h-none" : "max-h-30"
-                } overflow-hidden line-clamp-none`}
-              style={{ maxHeight: showFullContent ? "none" : "8rem" }}
-            >
-              <strong className="pb-3">{designation}{designation && <br></br>}</strong>
-              {content}
-              {!showFullContent && (
-                <div className="absolute bottom-0 left-0 w-full h-12 bg-gradient-to-t from-white to-transparent dark:from-blacksection" />
-              )}
+              </h2>
+              <div
+                className={`relative transition-max-height duration-500 ease-in-out ${showFullContent ? "max-h-none" : "max-h-30"
+                  } overflow-hidden line-clamp-none`}
+                style={{ maxHeight: showFullContent ? "none" : "8rem" }}
+              >
+                <strong className="pb-3">{designation}{designation && <br></br>}</strong>
+                {content}
+                {!showFullContent && (
+                  <div className="absolute bottom-0 left-0 w-full h-12 bg-gradient-to-t from-white to-transparent dark:from-blacksection" />
+                )}
+              </div>
+              <button
+                className="my-3 text-primaryho"
+                onClick={() => setShowFullContent(!showFullContent)}
+              >
+                {showFullContent ? "Show Less" : "Show More"}
+              </button>
             </div>
-            <button
-              className="my-3 text-primaryho"
-              onClick={() => setShowFullContent(!showFullContent)}
-            >
-              {showFullContent ? "Show Less" : "Show More"}
-            </button>
           </div>
         </div>
       </div>
-    </div>
     </motion.div>
   );
 };
--- components/Testimonial/index.tsx
+++ components/Testimonial/index.tsx
@@ -14,7 +14,7 @@ import SingleTestimonial from "./SingleTestimonial";
 const Testimonial = () => {
   return (
     <>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:my-10 md:mx-10 xl:mx-30">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 px-4 md:mt-5 md:px-8 2xl:px-0 max-w-c-1390 items-center mx-auto">
     {testimonialData.map((review) => (
           <SingleTestimonial key={review.id} review={review} />
         ))}
