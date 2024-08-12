3a29d18941d0f3449a1f6728648781a168563018
Zachary Artman <zacharyartman@icloud.com>
2024-08-11 21:45:56 -0700

    Fixed teachers line spacing

--- components/Testimonial/SingleTestimonial.tsx
+++ components/Testimonial/SingleTestimonial.tsx
@@ -14,18 +14,15 @@ const SingleTestimonial = ({ review }: { review: Testimonial }) => {
       <div className="px-12">
         <div className="mb-5 text-center border-b border-stroke dark:border-strokedark">
           <div>
-            <h3 className="mb-1.5 text-3xl text-black dark:text-white">
-              {name}
-            </h3>
-            <h2 className="mb-3">
-              <strong>{designation}</strong>
+            <h2 className="mb-1.5 text-3xl text-black dark:text-white">
+                {name}
             </h2>
             <div
-              className={`relative transition-max-height duration-500 ease-in-out ${
-                showFullContent ? "max-h-none" : "max-h-24"
-              } overflow-hidden line-clamp-none`}
-              style={{ maxHeight: showFullContent ? "none" : "6rem" }}
+              className={`relative transition-max-height duration-500 ease-in-out ${showFullContent ? "max-h-none" : "max-h-30"
+                } overflow-hidden line-clamp-none`}
+              style={{ maxHeight: showFullContent ? "none" : "8rem" }}
             >
+              <strong className="pb-3">{designation}{designation && <br></br>}</strong>
               {content}
               {!showFullContent && (
                 <div className="absolute bottom-0 left-0 w-full h-12 bg-gradient-to-t from-white to-transparent dark:from-blacksection" />
