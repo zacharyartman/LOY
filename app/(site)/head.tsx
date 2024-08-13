731c50d9e5e845c935b77160e48918734da74654
Zachary Artman <zacharyartman@icloud.com>
2024-08-13 11:58:15 -0700

    removed default meta tags

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,9 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title> Las Olas Yoga - Fort Lauderdale Yoga Studio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Fort Lauderdale non-heated yoga studio on Las Olas offering classes seven days a week for all levels. Classes include Foundations, Slow Flow, Flow & Restore." />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
