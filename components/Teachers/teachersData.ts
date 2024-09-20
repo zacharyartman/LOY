e54a426bd1ad36a0ff0b6d5662319e3e00683d64
Zachary Artman <zacharyartman@icloud.com>
2024-09-19 18:25:04 -0700

    made teacher ames alphabetical

--- components/Teachers/teachersData.ts
+++ components/Teachers/teachersData.ts
@@ -27,6 +27,13 @@ export const teachersData: Teacher[] = [
         content: "Keely first stepped into a yoga class in Syracuse, NY in 2011. Initially seeking the practice for its benefits in relieving stress, she quickly fell in love with the physical and spiritual benefits too and knew that she wanted yoga to be a part of her daily life. After moving to South Florida in 2013, Keely completed her RYT 200 and began teaching in studios, corporations, residences, and beach resorts. Her experience within the yoga community has included teaching, studio management, and operations management. Keely's classes are a balance of challenging transitions with a focus on strength and flexibility with a relaxed vibe encouraging self exploration and meditation. As a teacher Keely's goal is to provide a safe space for anyone who is looking to turn inward and explore the journey of yoga and to share the benefits that yoga has provided to her own life."
     },
     {
+        id: 11,
+        name: "Aly Gage",
+        designation: "",
+        image: image11,
+        content: "Aly began her yoga practice when she was 15 years old through a passion for cheerleading and movement. She immediately fell in love, and continued to casually practice yoga throughout her life. Last year, she decided to take her practice to the next level and applied for the 200 Hour teacher training hosted by Las Olas Yoga. LOY holds a special place in her heart, as it is the home of her very first role as a yoga teacher. From slow warm ups to dynamic flows, she always encourages students to honor their bodies and hearts and move with their own beautiful rhythm. When not on her yoga mat, Alyson is a structural engineer specializing in retail store design."
+    },
+    {
         id: 4,
         name: "Amy Baldacci",
         designation: "",
@@ -54,6 +61,13 @@ export const teachersData: Teacher[] = [
         content: "Emily was introduced to yoga in 2012 when she walked into a class at her local gym. At first she was drawn to the physicality of the practice but it didn't take long for her to realize the spiritual, mental and emotional benefits. Yoga quickly became the greatest way for her to practice self-care. Emily completed a 200 hour teacher training program in 2013 and spent the next 3 years teaching at various locations in her hometown of Brookfield, Wisconsin before moving to Fort Lauderdale in the spring of 2016. Upon moving to Fort Lauderdale, Emily began working and teaching at Ashtanga Yoga Worldwide. In 2018, she completed a year long training program under the guidance of world renowned yoga teacher Greg Nardi. Emily continues to experience the incredible benefits and healing powers of yoga through her daily practice and it brings her great joy to share the gift of yoga with others. She enjoys getting to know her students and believes in offering personalized instruction and guidance to fit each student's unique body and mind."
     },
     {
+        id: 12,
+        name: "Justin Liebman",
+        designation: "",
+        image: image12,
+        content: "Justin came to yoga after working as a top sales representative in the high stress industry of finance. Like many others who have found themselves drawn to this ancient practice, Justin’s passion for yoga ignited most when he began to study yogic philosophy. After four years of dedicated practice, he took the leap to become a teacher and leave the corporate world. Justin’s primary interest is in inversions and arm balances, both because they are challenging and also require a focus mind that creates internal stillness. Justin’s teachings reach beyond the physical aspects of yoga and has a strong passion for the subtle ones such as meditation and pranayama. He is a devoted student of yogic philosophy, and continues to see himself as a student, as he travels the world to gain more knowledge. Justin has completed multiple 500-hour yoga teacher trainings, received the FRC mobility certificate, and most recently, completed the Mindfulness Based Stress Reduction training."
+    },
+    {
         id: 9,
         name: "Tammy Patterson",
         designation: "",
@@ -66,20 +80,6 @@ export const teachersData: Teacher[] = [
         designation: "",
         image: image10,
         content: "Violeta's practice journey began 6 years ago finding the transformative power of yoga and its ability to bring harmony to the mind, body, and spirit. She completed Yoga Teacher Training in Bali where the gentle and patient approach resonated with her, creating a warm and welcoming space for her students to explore and embrace the practice of yoga."
-    },
-    {
-        id: 11,
-        name: "Aly Gage",
-        designation: "",
-        image: image11,
-        content: "Aly began her yoga practice when she was 15 years old through a passion for cheerleading and movement. She immediately fell in love, and continued to casually practice yoga throughout her life. Last year, she decided to take her practice to the next level and applied for the 200 Hour teacher training hosted by Las Olas Yoga. LOY holds a special place in her heart, as it is the home of her very first role as a yoga teacher. From slow warm ups to dynamic flows, she always encourages students to honor their bodies and hearts and move with their own beautiful rhythm. When not on her yoga mat, Alyson is a structural engineer specializing in retail store design."
-    },
-    {
-        id: 12,
-        name: "Justin Liebman",
-        designation: "",
-        image: image12,
-        content: "Justin came to yoga after working as a top sales representative in the high stress industry of finance. Like many others who have found themselves drawn to this ancient practice, Justin’s passion for yoga ignited most when he began to study yogic philosophy. After four years of dedicated practice, he took the leap to become a teacher and leave the corporate world. Justin’s primary interest is in inversions and arm balances, both because they are challenging and also require a focus mind that creates internal stillness. Justin’s teachings reach beyond the physical aspects of yoga and has a strong passion for the subtle ones such as meditation and pranayama. He is a devoted student of yogic philosophy, and continues to see himself as a student, as he travels the world to gain more knowledge. Justin has completed multiple 500-hour yoga teacher trainings, received the FRC mobility certificate, and most recently, completed the Mindfulness Based Stress Reduction training."
     },
     {
         id: 13,
