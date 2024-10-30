a7b8a841718aafd84e3e2e5d28768813bbea5e76
Zachary Artman <zacharyartman@icloud.com>
2024-10-29 19:14:17 -0700

    update pixel

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -34,7 +34,7 @@ export default function Head() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1584704052167393');
+            fbq('init', '527503170150662');
             fbq('track', 'PageView');
           `,
         }}
@@ -45,7 +45,7 @@ export default function Head() {
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=1584704052167393&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=527503170150662&ev=PageView&noscript=1"
         />
       </noscript>
     </>
