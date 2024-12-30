eda973e96607e81a11b4daa6cef5361f1f5033d5
Zachary Artman <zacharyartman@icloud.com>
2024-12-29 19:15:11 -0500

    update instagram svg

--- app/(site)/learn-more/page.tsx
+++ app/(site)/learn-more/page.tsx
@@ -49,25 +49,15 @@ const LearnMorePage = () => {
             </a>
           </li>
           <li>
-            <a href="https://instagram.com/lasolasyogastudio" aria-label="instagram icon">
-              <svg
-                className="fill-primary transition-all duration-300 hover:fill-primary"
-                width="30"
-                height="30"
-                viewBox="0 0 256 256"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clipPath="url(#clip0_48_1502)">
-                  <path
-                    d="M128,84a44,44,0,1,0,44,44A44.04978,44.04978,0,0,0,128,84Zm0,80a36,36,0,1,1,36-36A36.04061,36.04061,0,0,1,128,164ZM172,32H84A52.059,52.059,0,0,0,32,84v88a52.059,52.059,0,0,0,52,52h88a52.059,52.059,0,0,0,52-52V84A52.059,52.059,0,0,0,172,32Zm44,140a44.04978,44.04978,0,0,1-44,44H84a44.04978,44.04978,0,0,1-44-44V84A44.04978,44.04978,0,0,1,84,40h88a44.04978,44.04978,0,0,1,44,44ZM188,76a8,8,0,1,1-8-8A8.00917,8.00917,0,0,1,188,76Z"
-                    fill=""
-                    stroke='primary'
-                    strokeWidth="20"
-                  />
-                </g>
-              </svg>
-            </a>
+                    <a href="https://instagram.com/lasolasyogastudio" aria-label="Instagram icon" title="Instagram">
+                      <img
+                        src="/images/general/instagram.svg"
+                        alt="Instagram icon"
+                        className="fill-primary transition-all duration-300 hover:fill-primary"
+                        width="24"
+                        height="24"
+                      />
+                    </a>
           </li>
         </ul>
 
