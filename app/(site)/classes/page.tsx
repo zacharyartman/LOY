2d5c8fa05521d21c1418a564e282ee54ad6a756a
Zachary Artman <zacharyartman@icloud.com>
2025-05-03 07:12:54 -0700

    more page renames and updates to classes

--- app/(site)/classes/page.tsx
+++ app/(site)/classes/page.tsx
@@ -5,15 +5,26 @@ import Classes from "@/components/Classes";
 
 export const metadata: Metadata = {
   title: "Class Types | Yoga Classes for All Levels | Fort Lauderdale",
-  description: "We have classes for all levels: Foundations (beginners), Slow Flow (Flow at a slower pace), Flow (challenging practice) & Restore (calm, relaxing experience).",
+  description:
+    "We have classes for all levels: Foundations (beginners), Slow Flow (Flow at a slower pace), Flow (challenging practice) & Restore (calm, relaxing experience).",
   // other metadata
 };
 
 const ClassesPage = () => {
   return (
-    
     <>
-      <NewPageHero titleText={"Class Types"} link='/images/general/yoga-classes-near-me.webp'></NewPageHero>
+      <NewPageHero
+        titleText={"Class Types"}
+        link="/images/general/yoga-classes-near-me.webp"
+      ></NewPageHero>
+      <div className="flex items-center justify-center gap-2 py-7 xl:mt-0">
+        <a
+          href="/schedule"
+          className="flex items-center justify-center rounded-full bg-primary px-5.5 py-2.5 text-regular text-white transition-all duration-200 hover:bg-primaryho"
+        >
+          Book Now
+        </a>
+      </div>
       <Classes title={false}></Classes>
     </>
   );
--- app/(site)/schedule/page.tsx
+++ app/(site)/schedule/page.tsx
@@ -5,24 +5,32 @@ import MomenceSchedule from "@/components/MomenceSchedule";
 
 export const metadata: Metadata = {
   title: "Schedule | Best Yoga Classes | Fort Lauderdale",
-  description: "Weekly schedule of our yoga classes and workshops. We have classes 7 days a week for all levels with weekday morning & evening classes and weekend morning classes.",
+  description:
+    "Weekly schedule of our yoga classes and workshops. We have classes 7 days a week for all levels with weekday morning & evening classes and weekend morning classes.",
   // other metadata
 };
 
 const SchedulePage = () => {
   return (
-    
     <>
-      <NewPageHero titleText={"Schedule"} link='/images/hero/hero-schedule-yoga-near-me.webp'></NewPageHero>
-      <div className="pt-7 flex justify-center items-center gap-2 xl:mt-0">
-            <a
-              href="/classes"
-              className="flex items-center justify-center rounded-full bg-primary px-5.5 py-2.5 text-regular text-white transition-all duration-200 hover:bg-primaryho"
-            >
-              View Class Descriptions
-            </a>
-          </div>
-      <MomenceSchedule title={false} fullSchedule={true} sessionType={'class'} liteMode={false}/>
+      <NewPageHero
+        titleText={"Schedule"}
+        link="/images/hero/hero-schedule-yoga-near-me.webp"
+      ></NewPageHero>
+      <div className="flex items-center justify-center gap-2 pt-7 xl:mt-0">
+        <a
+          href="/classes"
+          className="flex items-center justify-center rounded-full bg-primary px-5.5 py-2.5 text-regular text-white transition-all duration-200 hover:bg-primaryho"
+        >
+          View Class Descriptions
+        </a>
+      </div>
+      <MomenceSchedule
+        title={false}
+        fullSchedule={true}
+        sessionType={"class"}
+        liteMode={false}
+      />
     </>
   );
 };
