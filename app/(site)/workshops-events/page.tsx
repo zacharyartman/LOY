86eb96236b617bc4e227765830dc8e1381fdb3c8
Zachary Artman <zacharyartman@icloud.com>
2025-05-03 08:23:22 -0700

    view class schedule on workshops page

--- app/(site)/workshops-events/page.tsx
+++ app/(site)/workshops-events/page.tsx
@@ -5,16 +5,33 @@ import MomenceSchedule from "@/components/MomenceSchedule";
 
 export const metadata: Metadata = {
   title: "Workshops | Yoga Classes | Fort Lauderdale",
-  description: "An overview of our upcoming yoga workshops and events at Las Olas Yoga in Fort Lauderdale. Founding members receive discounts on all events.",
+  description:
+    "An overview of our upcoming yoga workshops and events at Las Olas Yoga in Fort Lauderdale. Founding members receive discounts on all events.",
   // other metadata
 };
 
 const WorkshopsPage = () => {
   return (
-    
     <>
-      <NewPageHero titleText={"Workshops & Events"} link='/images/hero/hero-workshop-yoga-near-me.webp' position="bg-[center_80%]"></NewPageHero>
-      <MomenceSchedule title={false} fullSchedule={true} sessionType={'workshop'} liteMode={false}/>
+      <NewPageHero
+        titleText={"Workshops & Events"}
+        link="/images/hero/hero-workshop-yoga-near-me.webp"
+        position="bg-[center_80%]"
+      ></NewPageHero>
+      <div className="flex items-center justify-center gap-2 pt-7 xl:mt-0">
+        <a
+          href="/schedule"
+          className="flex items-center justify-center rounded-full bg-primary px-5.5 py-2.5 text-regular text-white transition-all duration-200 hover:bg-primaryho"
+        >
+          View Class Schedule
+        </a>
+      </div>
+      <MomenceSchedule
+        title={false}
+        fullSchedule={true}
+        sessionType={"workshop"}
+        liteMode={false}
+      />
     </>
   );
 };
