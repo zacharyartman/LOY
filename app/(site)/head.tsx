be6f87ddc2482283bb9fca5aac673c5f10dce25e
Zachary Artman <zacharyartman@icloud.com>
2025-05-12 19:01:58 -0700

    add additional config

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -2,22 +2,47 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-      <meta property="og:image" content="https://i0.wp.com/lasolas.yoga/wp-content/uploads/2021/11/loy-logo-brown-1.png" />
-      <link rel="manifest" href="/site.webmanifest"/>
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-      <meta name="msapplication-TileColor" content="#da532c"/>
-      <meta name="theme-color" content="#ffffff"/>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <meta
+        property="og:image"
+        content="https://i0.wp.com/lasolas.yoga/wp-content/uploads/2021/11/loy-logo-brown-1.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
 
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-39Y0SQQWM5"></script>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-591592888"
+      ></script>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-39Y0SQQWM5"
+      ></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+            gtag('config', 'AW-591592888');
             gtag('config', 'G-39Y0SQQWM5');
           `,
         }}
@@ -44,7 +69,7 @@ export default function Head() {
         <img
           height="1"
           width="1"
-          style={{ display: 'none' }}
+          style={{ display: "none" }}
           src="https://www.facebook.com/tr?id=527503170150662&ev=PageView&noscript=1"
         />
       </noscript>
