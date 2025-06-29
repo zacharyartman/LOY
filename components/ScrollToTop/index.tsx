ec4ba2da2255f480e3a9ced1b78fa54bbc4aecfe
Zachary Artman <zacharyartman@icloud.com>
2025-06-29 09:57:14 -0700

    update transition

--- components/ScrollToTop/index.tsx
+++ components/ScrollToTop/index.tsx
@@ -31,8 +31,8 @@ export default function ScrollToTop() {
         <div
           onClick={scrollToTop}
           aria-label="scroll to top"
-          className="hover:shadow-signUp flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80"
-          style={{ border: "3px solid rgba(255, 255, 255, 0.5)"}}
+          className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-primary text-white shadow-md transition duration-300 ease-in-out hover:scale-[1.05]"
+          style={{ border: "3px solid rgba(255, 255, 255, 0.5)" }}
         >
           <span className="mt-[6px] h-3 w-3 rotate-45 border-l border-t border-white"></span>
           <span className="sr-only">scroll to top</span>
