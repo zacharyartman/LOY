1636b3a27da4cf03ebb5ecbd2df5538c06936122
Zach Artman <zacharyartman@icloud.com>
2026-01-05 20:25:37 -0800

    update image names

--- components/Teachers/teachersData.ts
+++ components/Teachers/teachersData.ts
@@ -1,16 +1,16 @@
-import image5 from "@/public/images/user/carly.webp";
-import image17 from "@/public/images/user/dafna.webp";
-import image19 from "@/public/images/user/giuls.webp";
-import image1 from "@/public/images/user/heather.webp";
-import image12 from "@/public/images/user/justin.jpg";
-import image18 from "@/public/images/user/katesuarez.webp";
-import image2 from "@/public/images/user/keely.webp";
-import image22 from "@/public/images/user/melanie.webp"
-import image14 from "@/public/images/user/michael.jpeg";
-import image21 from "@/public/images/user/natalie.jpg";
-import image15 from "@/public/images/user/stacey_pic.jpeg";
-import image9 from "@/public/images/user/tammy.jpeg";
-import image20 from "@/public/images/user/tracy-wolfe.jpeg";
+import carly from "@/public/images/user/carly.webp";
+import dafna from "@/public/images/user/dafna.webp";
+import giuls from "@/public/images/user/giuls.webp";
+import heather from "@/public/images/user/heather.webp";
+import justin from "@/public/images/user/justin.jpg";
+import kate from "@/public/images/user/katesuarez.webp";
+import keely from "@/public/images/user/keely.webp";
+import melanie from "@/public/images/user/melanie.webp"
+import michael from "@/public/images/user/michael.jpeg";
+import natalie from "@/public/images/user/natalie.jpg";
+import stacey from "@/public/images/user/stacey_pic.jpeg";
+import tammy from "@/public/images/user/tammy.jpeg";
+import tracy from "@/public/images/user/tracy-wolfe.jpeg";
 import { Teacher } from "@/types/teacher";
 
 export const teachersData: Teacher[] = [
@@ -19,7 +19,7 @@ export const teachersData: Teacher[] = [
       "Heather's yoga journey began in 2005 in a corporate midday yoga class. Noticing how much better she was equipped to handle stress in the afternoons than in the mornings, she couldn't ignore the direct correlation to the practice. As Heather continued to practice regularly, the benefits went well beyond stress management and she decided to leave her corporate career of 20 years and dedicate her life to sharing the practice that changed her life with others. She began teaching yoga in 2012 and opened her first studio in 2015. Heather has taught thousands of classes and has led several teacher trainings. Each experience has equipped her with profound learnings and she is grateful for each one. Her mission at Las Olas Yoga and as a yoga instructor is to provide a space and a practice that invites students of all levels on a journey of self-exploration and self-discovery that they can take off the mat to lead a consciously-aware, loving and fulfilled life. Her approach to teaching yoga combines a focus on alignment with the freedom and creativity of a Vinyasa flow in a fun, relaxed environment. Her flow classes are meant to take students off of autopilot to allow them to drop fully into their practice. Her foundations classes are designed to be accessible to first-time students yet creative and unique for long-time students that prefer a slower practice, heavily rooted in alignment. For those that enjoy acronyms, Heather is an E-RYT 500 hour, YACEP certified yoga instructor.",
     designation: "Owner",
     id: 1,
-    image: image1,
+    image: heather,
     name: "Heather Artman",
   },
   {
@@ -27,7 +27,7 @@ export const teachersData: Teacher[] = [
       "Keely is a passionate and dedicated yoga teacher with over 10 years of experience. Through creative sequencing, mindful movement and breathwork, Keely encourages students to harness the power of presence, cultivating greater self-awareness and inner peace. Keely believes yoga is not just about physical postures but about fostering a self confidence that extends beyond the mat. Students leave Keely’s classes with a sense of inner strength and resilience, empowering them to approach life’s challenges with clarity and self-assurance. By integrating the physical benefits of yoga, Keely guides students to tap into their full potential, helping them live more vibrant and fulfilling lives, both on and off the mat.",
     designation: "Studio Manager",
     id: 2,
-    image: image2,
+    image: keely,
     name: "Keely Hubbard",
   },
   {
@@ -35,7 +35,7 @@ export const teachersData: Teacher[] = [
       "Inspired by her own yoga practice and a calling to help others, reinforced by a background in equity studies and a masters degree in alternative health, Carly began training to become a yoga teacher in 2022. Her mission as a teacher is to hold space for and welcome every body to the practices she guides; by offering longer holds and thoughtful sequencing, she makes yoga an accessible, compassionate, and magical practice where students can safely explore what they need each time they step onto their mats. From carefully curated playlists to intuitive hands-on adjustments, her dedication to each practitioner is palpable. Carly prioritizes empathy, inclusion, presence, and considerate details, leaving students feeling cared for and seen. In her class, you will experience an atmosphere of meditation, adaptability, and acceptance, from a trauma-sensitive and anatomy-informed perspective.",
     designation: "",
     id: 5,
-    image: image5,
+    image: carly,
     name: "Carly Lewis",
   },
   {
@@ -43,7 +43,7 @@ export const teachersData: Teacher[] = [
       "Dafna first discovered yoga while serving in the military, and it only took one class for her to be completely hooked. The focus, presence, and deep centering she experienced drew her in, sparking a desire to explore the practice further. Just one year later, she earned her teaching certification, and she has been guiding students ever since. With over nine years of teaching experience and more than 750 hours of training across various styles, Dafna brings a well-rounded and dynamic approach to her classes. She is passionate about every facet of yoga—meditation, breathwork, philosophy, and movement—and believes in both depth and breadth in practice. Her teaching philosophy emphasizes being caring rather than careful, encouraging students to step outside their comfort zones without fear but with curiosity and empowerment. Dafna meets students where they are, supporting their individual goals and intentions for their practice. In her flow classes, expect a mix of active mobility, unique and playful transitions, and strength-building holds. In her foundations classes, she breaks down fundamental postures with clear explanations and refinements. In her restorative classes, she guides students through long, supported stretch holds, breathwork, and guided meditations. No matter the class, Dafna creates a space for students to explore, grow, and connect—both on and off the mat.",
     designation: "",
     id: 17,
-    image: image17,
+    image: dafna,
     name: "Dafna Zacharin",
   },
   {
@@ -51,7 +51,7 @@ export const teachersData: Teacher[] = [
       "Giulia also known as Giuls is a Welsh / Italian yoga teacher based in Fort Lauderdale, Florida, with 15 years of dedicated practice. Her journey took a transformative turn in 2018 when she completed a 200-hour teacher training in Bali, Indonesia. Passionate about crafting dynamic and varied flows, Giuls encourages exploration and personal growth in each class. Community is at the heart of her teaching philosophy, being so far away from family and friends, Yoga has really helped her feel part of a community. As a certified prenatal yoga instructor, Giuls enjoys supporting expectant mothers on their unique journeys. It is a branch of Yoga she is very passionate about.",
     designation: "",
     id: 19,
-    image: image19,
+    image: giuls,
     name: "Giuls Mori",
   },
   {
@@ -59,7 +59,7 @@ export const teachersData: Teacher[] = [
       "Justin came to yoga after working as a top sales representative in the high stress industry of finance. Like many others who have found themselves drawn to this ancient practice, Justin’s passion for yoga ignited most when he began to study yogic philosophy. After four years of dedicated practice, he took the leap to become a teacher and leave the corporate world. Justin’s primary interest is in inversions and arm balances, both because they are challenging and also require a focus mind that creates internal stillness. Justin’s teachings reach beyond the physical aspects of yoga and has a strong passion for the subtle ones such as meditation and pranayama. He is a devoted student of yogic philosophy, and continues to see himself as a student, as he travels the world to gain more knowledge. Justin has completed multiple 500-hour yoga teacher trainings, received the FRC mobility certificate, and most recently, completed the Mindfulness Based Stress Reduction training.",
     designation: "",
     id: 12,
-    image: image12,
+    image: justin,
     name: "Justin Liebman",
   },
   {
@@ -67,7 +67,7 @@ export const teachersData: Teacher[] = [
       "Kate is a dedicated yoga and wellness enthusiast with over 9 years of experience, including founding and running her own cozy yoga studio. She thrives on creating welcoming spaces that nurture both mind and body, fostering a sense of community and mindfulness. Her approach is all about connecting with each individual, offering personalized guidance, and building lasting relationships. Kate is passionate about teaching students at every level, from beginners to advanced, helping them build strong foundations, refine their alignment, and explore advanced variations that are fun and feel good in their body.",
     designation: "",
     id: 18,
-    image: image18,
+    image: kate,
     name: "Kate Suarez",
   },
   {
@@ -75,7 +75,7 @@ export const teachersData: Teacher[] = [
       "Michael uses yoga as a metaphor for life, believing that a yoga journey should be fun and engaging while also challenging one to test and expand their limits. As a 500-hour Registered Yoga Instructor, he recognizes that he is teaching real people with their own stories. Yoga and meditation have made him a healthier, more aware person, opening his eyes to a world of positive opportunities, and he is truly grateful to share what he has learned with others. Each class he teaches has its own unique beat, vibration, and pulse. He focuses on alignment-based poses to deepen the practice, drawing from his dance background to create interesting and original sequences. This allows students to move authentically, finding their own rhythm and truth through movement.",
     designation: "",
     id: 14,
-    image: image14,
+    image: michael,
     name: "Michael Watson",
   },
   {
@@ -83,7 +83,7 @@ export const teachersData: Teacher[] = [
       "A former marketing and communications professional, Melanie was introduced to yoga when a friend invited her to a Yin Yoga class. As a former long-distance runner, she immediately resonated with the deep healing power of the practice. Her journey began with Hatha Yoga, which gave her a strong foundation before she discovered the fluidity of Vinyasa Flow, where movement and breath became one, and her connection to self and others deepened. Inspired by this transformation, Melanie completed her 200-hour yoga teacher training in February 2021. After a year of teaching, she immersed herself in a 300-hour advanced training, embracing a lifestyle rooted in daily meditation, asana, and spiritual growth. In the summer of 2025, Melanie manifested her dream of teaching yoga full-time. Melanie’s classes blend dynamic movement, intentional breathwork, and heartfelt connection. She guides students from a purely physical practice to one that embodies emotional depth and love — always infused with Metta (loving-kindness). Through themed classes, she shares her personal story and encourages others to explore their own path with courage and compassion. Now based in Fort Lauderdale with her husband and their two dogs, Riley and Ryatt, Melanie is a recent South Florida transplant from Northern California, where she raised her two sons, George and Will, and began her yoga journey. Off the mat, she’s an avid reader, cook, and outdoor enthusiast who brings passion and authenticity to all she does.",
     designation: "",
     id: 22,
-    image: image22,
+    image: melanie,
     name: "Melanie Duval",
   },
   {
@@ -91,7 +91,7 @@ export const teachersData: Teacher[] = [
       "Natalie is a 500hr RYT who began her practice in 2018 as a way to find balance and clarity during times of transition. Her teaching is heartfelt and down-to-earth, blending philosophy with playfulness to make yoga feel both grounding and uplifting. Inspired by her travels to Bali and India, Natalie shares yoga as a way to support others in meeting life’s ebbs and flows with strength, awareness, and presence.",
     designation: "",
     id: 21,
-    image: image21,
+    image: natalie,
     name: "Natalie Galliano",
   },
   {
@@ -99,7 +99,7 @@ export const teachersData: Teacher[] = [
       "Stacey began her yoga journey in 2000, practicing in the comfort of her living room with a DVD after the birth of her son. What started as a simple workout quickly became a profound and transformative practice that would change her life in ways she never anticipated. After years of deepening her own practice, Stacey received her 200-hour yoga certification in 2019, eager to share the benefits of yoga with others. As a teacher, Stacey creates a welcoming, soulful space for her students to explore and connect with their physical bodies, minds, and spirits. Her classes are a blend of fun, creativity, and mindful movement, designed to challenge students while honoring the importance of ease and relaxation. With an emphasis on balancing effort with surrender, Stacey encourages students to listen to their own bodies, cultivate awareness, and find joy on and off the mat. Whether you're new to yoga or a seasoned practitioner, Stacey's classes offer an opportunity to grow, heal, and tap into your own inner wisdom.",
     designation: "",
     id: 15,
-    image: image15,
+    image: stacey,
     name: "Stacey Ortense",
   },
   {
@@ -107,7 +107,7 @@ export const teachersData: Teacher[] = [
       "You will feel Tammy's heart before you hear her voice. This is why she loves yoga. Not only does yoga unite your mind, body, and spirit; but yoga also unites us as a community. Tammy believes that yoga is for everyone who walks through the door. In her classes, the goal is not to just ground and nurture your body through creative sequencing, but to also stimulate your mind and spirit through thought-provoking readings and meditations that help guide you towards inner peace. It is Tammy's hope that each of you will step off the mat feeling balanced, nourished, and full of life.",
     designation: "",
     id: 9,
-    image: image9,
+    image: tammy,
     name: "Tammy Patterson",
   },
   {
@@ -115,7 +115,7 @@ export const teachersData: Teacher[] = [
       "With over 25 years of personal yoga practice, Tracy brings both depth of experience and a heartfelt passion for sharing yoga with others. While working as a commercial lawyer for 14 years, yoga was her sanctuary—a way to relieve stress, reconnect with herself, and find balance amidst the demands of the legal practice. After stepping away from law, she completed her 200-hour teacher training program in Chicago and began teaching in several suburban studios and relocated to South Florida in 2024. Tracy’s classes weave together breath, movement, and mindfulness, encouraging students to feel grounded, intentional, and confident. She loves guiding students to “get lost” in their practice—moving fluidly while staying deeply present. Yoga has profoundly changed her life, and she is grateful for the opportunity to share its transformative gifts with her students.",
     designation: "",
     id: 20,
-    image: image20,
+    image: tracy,
     name: "Tracy Wolfe",
   },
 ];
