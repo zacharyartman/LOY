bdcbd12f94518ac5ab715a73c44455171268d21b
Zach Artman <zacharyartman@icloud.com>
2026-03-30 19:33:23 -0700

    fix learn more page

--- app/(site)/learn-more/page.tsx
+++ app/(site)/learn-more/page.tsx
@@ -12,64 +12,59 @@ export const metadata: Metadata = {
 
 const LearnMorePage = () => {
   return (
-    <div className="mb-10">
-      <section className="relative mt-25 h-[450px] overflow-hidden md:pt-40 xl:pb-25 xl:pt-46">
-        <div className="absolute inset-0 bg-[url('/images/hero/hero-yoga-near-me.jpeg')] bg-cover bg-center"></div>
-        <div className="absolute bottom-0 left-0 right-0 h-[200px] bg-gradient-to-t from-midbrown to-transparent">
-          <div className="relative z-10 flex h-full items-end justify-center pb-5">
-            <h1 className="text-center text-sectiontitle2 font-bold text-primary xl:text-hero">
-              Las Olas Yoga
-            </h1>
+    <div>
+      <section className="relative mt-[88px] bg-midbrown pb-20 md:pb-30">
+        <div className="relative h-[400px] lg:h-[480px]">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="h-full w-full bg-[url('/images/hero/hero-yoga-near-me.jpeg')] bg-cover bg-top lg:bg-center"></div>
+          </div>
+          <div className="absolute bottom-[-80px] left-0 right-0 h-[340px] bg-gradient-to-t from-midbrown via-midbrown/70 to-transparent">
+            <div className="relative z-10 flex h-full flex-col items-center justify-end gap-3 pb-[100px]">
+              <h1 className="text-center text-sectiontitle2 font-bold text-primary xl:text-hero">
+                Las Olas Yoga
+              </h1>
+              <ul className="flex items-center justify-center gap-3">
+                <li>
+                  <a href="https://www.facebook.com/lasolasyoga" aria-label="facebook icon">
+                    <svg
+                      className="fill-primary transition-all duration-300 hover:fill-primary"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clipPath="url(#clip0_48_1499)">
+                        <path
+                          d="M14 13.5H16.5L17.5 9.5H14V7.5C14 6.47 14 5.5 16 5.5H17.5V2.14C17.174 2.097 15.943 2 14.643 2C11.928 2 10 3.657 10 6.7V9.5H7V13.5H10V22H14V13.5Z"
+                          fill=""
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_48_1499">
+                          <rect width="24" height="24" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://instagram.com/lasolasyogastudio" aria-label="Instagram icon" title="Instagram">
+                    <img
+                      src="/images/general/instagram.svg"
+                      alt="Instagram icon"
+                      className="fill-primary transition-all duration-300 hover:fill-primary"
+                      width="24"
+                      height="24"
+                    />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </section>
-      <section className="relative h-auto bg-midbrown pb-20 md:pb-30">
-        <ul className="flex items-center justify-center gap-3">
-          <li>
-            <a
-              href="https://www.facebook.com/lasolasyoga"
-              aria-label="facebook icon"
-            >
-              <svg
-                className="fill-primary transition-all duration-300 hover:fill-primary"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clipPath="url(#clip0_48_1499)">
-                  <path
-                    d="M14 13.5H16.5L17.5 9.5H14V7.5C14 6.47 14 5.5 16 5.5H17.5V2.14C17.174 2.097 15.943 2 14.643 2C11.928 2 10 3.657 10 6.7V9.5H7V13.5H10V22H14V13.5Z"
-                    fill=""
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_48_1499">
-                    <rect width="24" height="24" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://instagram.com/lasolasyogastudio"
-              aria-label="Instagram icon"
-              title="Instagram"
-            >
-              <img
-                src="/images/general/instagram.svg"
-                alt="Instagram icon"
-                className="fill-primary transition-all duration-300 hover:fill-primary"
-                width="24"
-                height="24"
-              />
-            </a>
-          </li>
-        </ul>
 
-        <div className="relative mx-auto max-w-[800px] pt-8">
+        <div className="relative mx-auto max-w-[800px] pt-5">
           <Offers />
         </div>
         {/* <div className="absolute bottom-0 left-0 right-0 h-[200px] pointer-events-none bg-gradient-to-t from-white to-transparent"></div> */}
